71bc3f17999969c632e4a398563fedb92fa27b65
JedrzejSowa <01197856@pw.edu.pl>
2024-12-27 16:24:06 +0100

    Update struktrury.h
    dodanie zarysu struktur

---
+++ struktrury.h
@@ -0,0 +1,27 @@
+#ifndef STRUKTURY_H
+#define STRUKTURY_H
+
+
+typedef struct board {
+    int PosX;
+    int PosY;
+    int IsMine;
+
+    struct board *left;
+    struct board *right;
+    struct board *up;
+    struct board *down;
+    struct board *lup;
+    struct board *pup;
+    struct board *ldown;
+    struct board *pdown;
+} board;
+
+typedef struct PlayBoard {
+    int PosX;
+    int PosY;
+    char output;
+} PlayBoard
+
+
+#endif 
