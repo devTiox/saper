00e1e495d37e110380de92015118e0a99906e3a3
JedrzejSowa <01197856@pw.edu.pl>
2024-12-29 17:53:28 +0100

    dodałem bo nie było

---
+++ generator_min.h
@@ -0,0 +1,9 @@
+#ifndef GENERATOR_MIN_H
+#define GENERATOR_MIN_H
+#include "struktury.h"
+
+void rozmiesc_bomby(int l_bomb,int y,int x,board**pole);
+
+int sprawdzenie(int y,int x,board**pole);
+
+#endif
---
+++ ruchy.h
@@ -0,0 +1,7 @@
+#ifndef RUCHY_H
+#define RUCHY_H
+#include "struktury.h"
+
+void ruch(PlayBoard**Plansza);
+
+#endif
