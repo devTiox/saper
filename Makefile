c76a9b642a35470c6da7ffcdd120e838860dea5e
JedrzejSowa <01197856@pw.edu.pl>
2025-01-25 17:38:21 +0100

    tabela wyników

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC=gcc
 saper: main.o ruchy.o generator_min.o struktury.o wypisywanie.o
-	$(CC) main.o ruchy.o generator_min.o struktury.o wypisywanie.o -o saper
+	$(CC) main.o ruchy.o generator_min.o struktury.o wypisywanie.o tabela.o -o saper
 
-main.o: ruchy.o generator_min.o struktury.o wypisywanie.o
+main.o: ruchy.o generator_min.o struktury.o wypisywanie.o tabela.o main.c
 	$(CC) -c main.c
 
 generator_min.o: generator_min.c generator_min.h struktury.o
@@ -16,6 +16,9 @@ wypisywanie.o: wypisywanie.c wypisywanie.h struktury.o
 
 struktury.o: struktury.c struktury.h
 	$(CC) -c struktury.c
+
+tabela.o: tabela.c tabela.h
+	$(CC) -c tabela.c
 
 clean:
 	rm *.o saper
--- main.c
+++ main.c
@@ -5,25 +5,30 @@
 #include "wypisywanie.h"
 #include "ruchy.h"
 #include "generator_min.h"
+#include "tabela.h"
 
 int BOOM=1;
 
 int main(int argc,char**argv)
 {
+	char nick[100];
+    printf("Podaj nick: ");
+    if (fgets(nick, sizeof(nick), stdin) != NULL) {
+        size_t len = strlen(nick);
+        if (len > 0 && nick[len-1] == '\n') {
+            nick[len-1] = '\0';
+        }
+    }
+    printf("Twoj nick: %s\n", nick);
 	int l_bomb;
 	int y, x;
 	char move;
 	int mnoznik;
 	int pkt=0;
 	int max_pkt;
-	int opt;
-	if(opt = getopt(argc ,argv, "f:"))
-	{
-		FILE*plik=fopen(optarg,"r");
-		fscanf(plik,"%d %d",&pkt,&max_pkt);
-		printf("%d %d\n",pkt , max_pkt);
-		return 0;
-	}
+	int count;
+	
+
 	wybor_poziomu(&l_bomb,&y,&x,move,&mnoznik);
 	max_pkt=((x*y)-l_bomb)*mnoznik;
 	PlayBoard**Plansza=stworz_plansze(y,x);
@@ -47,6 +52,11 @@ int main(int argc,char**argv)
 
 	}
 	printf("Koniec gry.Liczba zdobytych punktow:%d\n",pkt);
-	
+	Tabela wyniki[10];
+	czytaj_wyniki(wyniki, &count);
+	zmien_wyniki(wyniki, &count, nick, pkt);
+	zapisz_wyniki(wyniki, count);
+
+
 	return 0;
 }
